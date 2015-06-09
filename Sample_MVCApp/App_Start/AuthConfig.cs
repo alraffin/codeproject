4d6339bcac3ab805d14348f1d444979b9f9b1d4d
alraffin <alraffin@ipb.fr>
2015-06-09 14:03:20 -0500

    trigger test

--- Sample_MVCApp/App_Start/AuthConfig.cs
+++ Sample_MVCApp/App_Start/AuthConfig.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Web.WebPages.OAuth;
 using Sample_MVCApp.Models;
-
+// useless comment to test triggers
 namespace Sample_MVCApp
 {
     public static class AuthConfig
