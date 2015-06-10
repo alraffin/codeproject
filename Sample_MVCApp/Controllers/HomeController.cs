13950502ef8771ee4faf2a79ca47534bdd44fdc7
alraffin <alraffin@ipb.fr>
2015-06-10 16:08:34 -0500

    test 1

--- Sample_MVCApp/Controllers/HomeController.cs
+++ Sample_MVCApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace Sample_MVCApp.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application. Hello, it's working";
 
             return View();
         }
